9ad5848cb0772dd54597d847ef5f3549bbb3a820
desecnd <desecnd@protonmail.com>
2023-11-11 20:45:52 +0100

    Refactor SynthStream and add class probabilities

--- src/synthstream/synthstream.py
+++ src/synthstream/synthstream.py
@@ -8,33 +8,72 @@ class ActiveLabelDuplicateErorr(Exception):
     pass
 
 class SyntheticStream:
-
-    def __init__(self, size: int = None, seed: int = None, init_csamplers: list[ClassSampler] = []):
-        self.active_samplers = dict()
-        self.future_samplers = list()
-        self.t = 0 # next_t ?
-        self.n = size
+    def __init__(self, max_samples: int = None, seed: int = None, init_csamplers: list[ClassSampler] = []):
         self._random = Random(seed)
-        self.curr_x = None
-        self.curr_y = None
+        self.max_samples = max_samples
+        self.active_samplers = dict()
+        self.future_samplers: list(csampler) = []
+        self.next_t = 0 
+        self.all_labels_set: set(str) = set()
+
+        self.last_class_probabilities = None
+        self.last_sample = None
+        self.last_label = None
         
         for csampler in init_csamplers: 
             self.add_csampler(csampler)
-            
-    # how to deal with labels
-    def _activate_csampler(self, csampler):
-        if csampler.label in self.active_samplers.keys():
-            raise ActiveLabelDuplicateErorr
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self) -> tuple:
+        self._advance_state()
+        return self.output
+
+    @property
+    def output(self) -> tuple:
+        return (self.last_sample, self.last_label)
+    
+    @property
+    def class_probabilities(self) -> dict:
+        return self.last_class_probabilities
         
-        # arm for the first use
-        print(f"[.]: Activating class sampler {csampler.label} at time {self.t}.")
+    def add_csampler(self, csampler: ClassSampler):
+        if csampler.stream_t_start < self.next_t:
+            raise ValueError("Trying to add class sampler in the past")
+        elif csampler.stream_t_start == self.next_t:
+            self._activate_csampler(csampler)
+        elif csampler.stream_t_start > self.next_t:
+            self._add_future_csampler(csampler)
+            
+        self.all_labels_set.add(csampler.label)
+        
+    """
+    def _calc_curr_class_probs(self) -> dict:
+        label_weight_pairs = []
+        weight_sum = 0.0
+        for label in self.all_labels_set:
+            if label in self.active_samplers.keys():
+                csampler = self.active_samplers[label]
+                weight_sum += csampler.curr_weight 
+                label_weight_pairs.append((label, csampler.curr_weight))
+        normalized_pairs = [ (label, w/weight_sum) for label, w in label_weight_pairs ]
+        return dict(normalized_pairs)
+    """
+
+    def _activate_csampler(self, csampler):
+        """Add `csampler` to dictionary of active samplers - currently used in a stream"""
+        if csampler.label in self.active_samplers.keys():
+            raise ActiveLabelDuplicateErorr(f"Class {csampler.label} is already present as active")
+        
+        print(f"[.]: Activating class sampler {csampler.label} at time {self.next_t}.")
         csampler.next_state()
         self.active_samplers[csampler.label] = csampler
 
     def _remove_active_csampler(self, csampler):
         assert csampler.label in self.active_samplers.keys()
         del self.active_samplers[csampler.label]
-        print(f"[.]: Removing active class sampler {csampler.label} at time {self.t}.")
+        print(f"[.]: Removing active class sampler {csampler.label} at time {self.next_t}.")
         
     def _add_future_csampler(self, csampler):
         """Add `csampler` with `stream_t_start` > current stream `t`, to use-in-the-future samplers list """
@@ -53,62 +92,51 @@ class SyntheticStream:
                 "from one class, in such case `SyntheticStream` will raise `ActiveClassDuplicateError.`"
             )
             
-        assert csampler.stream_t_start > self.t
+        assert csampler.stream_t_start > self.next_t
         self.future_samplers.append(csampler)
-     
-    def add_csampler(self, csampler: ClassSampler):
-        # maybe merge <= ? what if we try to add to the past?
-        if csampler.stream_t_start < self.t:
-            raise ValueError("Trying to add class sampler in the past")
-        elif csampler.stream_t_start == self.t:
-            self._activate_csampler(csampler)
-        elif csampler.stream_t_start > self.t:
-            self._add_future_csampler(csampler)
     
-    def next_state(self):
-        if self.t == self.n:
+    def _advance_state(self):
+        # raise StopIteration to correctly handle `for x in stream` syntax
+        if self.next_t == self.max_samples:
             raise StopIteration
         
+        # raise Exception if there is no samplers to sample from
         if len(self.active_samplers.items()) == 0:
-            raise NoActiveSamplersError
+            raise NoActiveSamplersError(f"[!]: No more samplers in the stream at time {self.next_t}")
         
-        weights = []
-        data = []
+        weights = [ csampler.get_state()[0] for csampler in self.active_samplers.values() ]
+        active_csamplers = [ csampler for csampler in  self.active_samplers.values() ]
+        
+        weight_sum = sum(weights)
 
-        for csampler in self.active_samplers.values():
-            w, x = csampler.get_state()
-            weights.append(w)
-            data.append((x, csampler))
-            
-        win_x, win_csampler = self._random.choices(data, weights, k=1)[0]
+        # Store results as probabilities inside dictionary: label -> prob
+        self.last_class_probabilities = dict(
+            [(csampler.label, w / weight_sum) for w, csampler in zip(weights, active_csamplers)]
+        )
 
-        # TODO: maybe think of better way to store the results?
-        self.curr_x = win_x
-        self.curr_y = win_csampler.label
+        # fill not used classes with probability 0.0
+        for label in self.all_labels_set:
+            if label not in self.last_class_probabilities:
+                self.last_class_probabilities[label] = 0.0
 
-        # arm for the next state, or remove from current one if ended
-        # this may raise `EndOfClassError` and it is OK. 
+        selected_csampler = self._random.choices(active_csamplers, weights, k=1)[0]
+
+        self.last_sample = selected_csampler.curr_sample
+        self.last_label = selected_csampler.label
+
+        # This may raise `EndOfClassError` and it is OK. 
         # It means it is an Error that user was prepared for while declaring ClassSampler strategy
         try:
-            win_csampler.next_state()
+            selected_csampler.next_state()
         except EndOfClassSamples as e:
-            # delete from the stream
-            self._remove_active_csampler(win_csampler)
+            # This means we specified `n_samples` param inside ClassSampler
+            # deactivate because we already sampled `n_samples` from it.
+            self._remove_active_csampler(selected_csampler)
             
-        # state transition
-        self.t += 1
-
-        # activate future classes 
+        # If future sampler is meant to start next round, activate it
         for csampler in self.future_samplers:
-            if csampler.stream_t_start == self.t:
+            if csampler.stream_t_start == self.next_t + 1:
                 self._activate_csampler(csampler)
-                
-    def get_state(self):
-        return self.curr_x, self.curr_y
 
-    def __iter__(self):
-        return self
-    
-    def __next__(self):
-        self.next_state()
-        return self.get_state()
+        self.next_t += 1
+        
--- tests/test_synthstream.py
+++ tests/test_synthstream.py
@@ -1,4 +1,4 @@
-from synthstream.synthstream import SyntheticStream
+from synthstream.synthstream import SyntheticStream, NoActiveSamplersError
 from synthstream.csampler import ClassSampler
 import pytest
 
@@ -18,17 +18,79 @@ class TestSyntheticStream:
             stream_t_start=2, n_samples=500, eoc_strategy='loop'
         )
 
-        ss = SyntheticStream(size=None, seed=42, init_csamplers=[cs1, cs2])
+        ss = SyntheticStream(max_samples=None, seed=42, init_csamplers=[cs1, cs2])
+
+        assert next(ss) == (ex1[0], 'A') 
+        assert next(ss) == (ex1[1], 'A')
+        assert next(ss) == (ex2[0], 'B')
+        assert next(ss) == (ex2[1], 'B')
+        assert next(ss) == (ex2[0], 'B')
+
+    def test_stream_end_of_iteration(self): 
+        none_sampler = ClassSampler(
+            label='None', samples=[], weight_func=lambda t: 5, 
+            stream_t_start=0, n_samples=10, eoc_strategy='none'
+        )
         
-        ss.next_state()
-        assert ss.get_state() == (ex1[0], 'A' )
-        ss.next_state()
-        assert ss.get_state() == (ex1[1], 'A' )
-        ss.next_state()
-        assert ss.get_state() == (ex2[0], 'B' )
-        ss.next_state()
-        assert ss.get_state() == (ex2[1], 'B' )
-        ss.next_state()
-        assert ss.get_state() == (ex2[0], 'B' )
-                
+        ss = SyntheticStream(max_samples=2, seed=42, init_csamplers=[none_sampler])
+
+        assert next(ss)[0] is None
+        assert next(ss)[0] is None
+        
+        with pytest.raises(StopIteration) as e:
+            next(ss)
+            
+    def test_iterator_interface_correctness(self):
+        none_sampler = ClassSampler(
+            label='None', samples=[], weight_func=lambda t: 1, 
+            n_samples=20, eoc_strategy='none'
+        )
+        
+        ss = SyntheticStream(max_samples=10, init_csamplers=[none_sampler])
+        
+        assert next(ss) == (None, 'None')
+        # 9 samples left in the stream
+        assert sum([1 for _ in ss]) == 9
+
+        # trying to read from ended stream
+        with pytest.raises(StopIteration):
+            next(ss)
+            
+    def test_synthstream_end_of_classsamplers(self):
+        none_sampler = ClassSampler(
+            label='None', samples=[], weight_func=lambda t: 1,
+            n_samples=10, eoc_strategy='none'
+        )
+        
+        ss = SyntheticStream(max_samples=20)
+        ss.add_csampler(none_sampler)
+        
+        for i in range(10):
+            next(ss) == (None, 'None')
+            
+        with pytest.raises(NoActiveSamplersError) as e:
+            next(ss)
+        
+    def test_class_sampler_with_raise_eoc_raises_exception(self):
+        raise_sampler = ClassSampler(
+            label = 'TEST', samples=[1] * 20, weight_func=lambda t: 1,
+        )
+    
+    def test_valid_class_probabilities_in_stream(self):
+        FLOAT_EPS = 1e-9
+        sampler_a = ClassSampler(
+            label = 'A', samples=[1] * 20, weight_func=lambda t: 10, n_samples=2
+        )
+        sampler_b = ClassSampler(
+            label= 'B', samples=[2] * 20, weight_func=lambda t: 20,
+            stream_t_start=1, n_samples=1
+        )
+        
+        ss = SyntheticStream(seed=42, init_csamplers=[sampler_a, sampler_b])
+        assert ss.class_probabilities is None
+        assert next(ss) == (1, 'A')
+        assert abs(ss.class_probabilities['A'] - 1.0) < FLOAT_EPS 
+        assert ss.class_probabilities['B'] < FLOAT_EPS
+
+        
 
