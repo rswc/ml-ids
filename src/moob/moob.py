516a808ae39a15a6be1bda5da8df227a2f0cbb0d
AleksanderBak <aleksanderbak9@gmail.com>
2023-12-15 13:04:06 +0100

    Adding unit tests

--- src/moob/moob.py
+++ src/moob/moob.py
@@ -9,21 +9,28 @@ class MOOB(ensemble.BaggingClassifier):
             n_models=10,
             seed=None,
             decay_factor=0.9,
+            resampling="oversampling"
     ) -> None:
         super().__init__(model=model, n_models=n_models, seed=seed)
-        self.decay_factor = decay_factor
-        self.class_priors: dict[base.typing.ClfTarget, float] = {}
+        self.decay_factor: float = decay_factor
+        self._class_priors: dict[base.typing.ClfTarget, float] = {}
+        self.resampling: str = resampling
 
     def learn_one(self, x, y, **kwargs):
-        if y not in self.class_priors:
-            self.class_priors[y] = 0
+        if y not in self._class_priors:
+            self._class_priors[y] = 0
 
-        for cls in self.class_priors:
-            self.class_priors[cls] = self.decay_factor * self.class_priors[cls] + ((1 - self.decay_factor) * (y == cls))
+        for cls in self._class_priors:
+            self._class_priors[cls] = self.decay_factor * self._class_priors[cls] + ((1 - self.decay_factor) * (y == cls))
 
-        w_max = max(self.class_priors.values())
-
-        for model in self:
-            for _ in range(utils.random.poisson(w_max / self.class_priors[y], self._rng)):
-                model.learn_one(x, y, **kwargs)
+        if self.resampling == "undersampling":
+            min_prior = min(self._class_priors.values())
+            for model in self:
+                for _ in range(utils.random.poisson(min_prior / self._class_priors[y], self._rng)):
+                    model.learn_one(x, y, **kwargs)
+        else:
+            max_prior = max(self._class_priors.values())
+            for model in self:
+                for _ in range(utils.random.poisson(max_prior / self._class_priors[y], self._rng)):
+                    model.learn_one(x, y, **kwargs)
         return self
