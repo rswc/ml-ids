fd66ce038f6c286c39775017272cdca929cdb003
rswc <rshoryti@protonmail.com>
2023-12-25 01:40:45 +0100

    Add per-class metrics adapter mixin

--- src/framework/adapters/model.py
+++ src/framework/adapters/model.py
@@ -1,20 +1,26 @@
 from .base import ModelAdapterBase
+from .mixin import PerClassMetricsMixin
 from cbce import CBCE
 from river.forest import ARFClassifier
 
 def extract_mean(prop, models):
     return sum(map(lambda m: getattr(m, prop, 0.0), models)) / len(models)
 
-class CBCEAdapter(ModelAdapterBase[CBCE]):
+class CBCEAdapter(PerClassMetricsMixin, ModelAdapterBase[CBCE]):
 
     @classmethod
     def get_target_class(self):
         return CBCE
     
+    def _get_inner_classifiers(self) -> dict:
+        return self._model.classifiers
+    
     def get_loggable_state(self) -> dict:
-        return {
+        state = {
             "class_priors": self._model._class_priors
         }
+
+        return self.add_per_class_state(state)
 
 class ARFAdapter(ModelAdapterBase[ARFClassifier]):
 
