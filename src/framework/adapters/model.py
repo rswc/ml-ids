dd203b9afce479f26e5f52ca50930decb109127c
AleksanderBak <aleksanderbak9@gmail.com>
2024-01-03 13:32:02 +0100

    Adding RBC adapter

--- src/framework/adapters/model.py
+++ src/framework/adapters/model.py
@@ -3,9 +3,12 @@ from .base import ModelAdapterBase, DriftModelAdapterBase
 from .mixin import PerClassMetricsMixin
 from cbce import CBCE
 from river.forest import ARFClassifier
+from rbc import ResamplingBaggingClassifier
+
 
 def extract_mean(prop, models):
     return sum(map(lambda m: getattr(m, prop, 0.0), models)) / len(models)
+
 
 class CBCEAdapter(PerClassMetricsMixin, DriftModelAdapterBase[CBCE]):
 
@@ -36,6 +39,7 @@ class CBCEAdapter(PerClassMetricsMixin, DriftModelAdapterBase[CBCE]):
 
         return self.add_per_class_state(state)
 
+
 class ARFAdapter(PerClassMetricsMixin, ModelAdapterBase[ARFClassifier]):
 
     @classmethod
@@ -49,6 +53,23 @@ class ARFAdapter(PerClassMetricsMixin, ModelAdapterBase[ARFClassifier]):
         state = {
             "mean_active_leaves": extract_mean("_n_active_leaves", self.model._background),
             "mean_inactive_leaves": extract_mean("_n_inactive_leaves", self.model._background),
+        }
+
+        return self.add_per_class_state(state)
+
+
+class RBCAdapter(PerClassMetricsMixin, ModelAdapterBase[ResamplingBaggingClassifier]):
+
+    @classmethod
+    def get_target_class(self) -> type[ResamplingBaggingClassifier]:
+        return ResamplingBaggingClassifier
+
+    def _get_inner_classifiers(self) -> dict:
+        return self._model.models
+
+    def get_loggable_state(self) -> dict:
+        state = {
+            "class_priors": self._model._class_priors,
         }
 
         return self.add_per_class_state(state)
